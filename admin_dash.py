53cffb008ffa302f984784375aa8423f62e0f34b
SENSEI-9 <78782813+SENSEI-9@users.noreply.github.com>
2021-09-06 22:48:31 +0545

    commit

--- admin_dash.py
+++ admin_dash.py
@@ -597,6 +597,7 @@ class admin:
         self.displaycustom['show'] = 'headings'
 
 
+
         self.displaycustom.column("username", width=100)
         self.displaycustom.column("spanich", width=100)
         self.displaycustom.column("tomato", width=100)
